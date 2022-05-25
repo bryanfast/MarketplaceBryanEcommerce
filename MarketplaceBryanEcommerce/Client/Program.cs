688036b7cffd9fba6371ce7240ff52b841206915
Bryan Marin <bmvingeniero@gmail.com>
2022-05-24 19:59:43 -0500

    Primera parte implementacion del Carrito de Compras

--- MarketplaceBryanEcommerce/Client/Program.cs
+++ MarketplaceBryanEcommerce/Client/Program.cs
@@ -1,5 +1,7 @@
 global using MarketplaceBryanEcommerce.Shared;
 global using System.Net.Http.Json;
+global using Blazored.LocalStorage;
+global using MarketplaceBryanEcommerce.Client.Services.CartService;
 global using MarketplaceBryanEcommerce.Client.Services.CategoryService;
 global using MarketplaceBryanEcommerce.Client.Services.ProductService;
 using MarketplaceBryanEcommerce.Client;
@@ -9,9 +11,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+// Servicio de Local Storage
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
