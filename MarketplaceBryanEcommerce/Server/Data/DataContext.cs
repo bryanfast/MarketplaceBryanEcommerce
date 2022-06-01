624e6d236964f2067675a0a06e1f75b8d10655ea
Bryan Marin <bmvingeniero@gmail.com>
2022-06-01 09:48:10 -0500

    Se agrega modelo orders.

--- MarketplaceBryanEcommerce/Server/Data/DataContext.cs
+++ MarketplaceBryanEcommerce/Server/Data/DataContext.cs
@@ -10,11 +10,14 @@ namespace MarketplaceBryanEcommerce.Server.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CartItem>().HasKey(
-                ci => new { ci.UserId,ci.ProductId, ci.ProductTypeId }
+                ci => new { ci.UserId, ci.ProductId, ci.ProductTypeId }
                 );
             modelBuilder.Entity<ProductVariant>().HasKey(
                 p => new { p.ProductId, p.ProductTypeId }
                 );
+            modelBuilder.Entity<OrderItem>().HasKey(
+                oi => new { oi.OrderId,oi.ProductId, oi.ProductTypeId}
+            );
 
             modelBuilder.Entity<ProductType>().HasData(
                 new ProductType { Id = 1, Name = "Defautl" },
@@ -139,5 +142,7 @@ namespace MarketplaceBryanEcommerce.Server.Data
         public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
